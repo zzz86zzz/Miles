656a9f9e83b2f95b01fe4f7b3e95894f7b88fe0d
Oleg Shulga <oleg021086@gmail.com>
2021-01-06 23:58:41 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 325_000;
+        int constant  = 20;
+        int miles = price / constant;
+        System.out.println(miles);
+    }
+}
